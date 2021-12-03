94f0429cbdec7d535c66f32f98f19494e87f306c
J-Human <justinleeong@gmail.com>
2021-12-03 14:14:51 +0800

    December 2, part 1 and 2

---
+++ 2021/2/1/main.cpp
@@ -0,0 +1,34 @@
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+int main() {
+	std::string data;
+	std::ifstream file;
+
+	file.open("2021/2/input.txt");
+	if (!file) {
+		std::cerr << "Failed to open input file." << std::endl;
+		return 1;
+	}
+
+	int depth = 0, horizontal = 0;
+	while (std::getline(file, data)) {
+		int distance = data.back() - '0';
+		switch (data.front()) {
+			case 'd':
+				depth += distance;
+				break;
+			case 'f':
+				horizontal += distance;
+				break;
+			case 'u':
+				depth -= distance;
+				break;
+		}
+	}
+	file.close();
+
+	std::cout << depth * horizontal << std::endl;
+}
---
+++ 2021/2/2/main.cpp
@@ -0,0 +1,36 @@
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+int main() {
+	std::string data;
+	std::ifstream file;
+
+	file.open("2021/2/input.txt");
+	if (!file) {
+		std::cerr << "Failed to open input file." << std::endl;
+		return 1;
+	}
+
+	int aim = 0, depth = 0, horizontal = 0;
+	while (std::getline(file, data)) {
+		int amount = data.back() - '0';
+		switch (data.front()) {
+			case 'd':
+				aim += amount;
+				break;
+			case 'f':
+				horizontal += amount;
+				depth += aim * amount;
+				break;
+			case 'u':
+				aim -= amount;
+				break;
+		}
+	}
+	file.close();
+
+	std::cout << depth * horizontal << std::endl;
+}
