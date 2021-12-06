294f3a45e6f47627b1c0f85f957406b2cb0354ca
J-Human <justinleeong@gmail.com>
2021-12-06 21:15:00 +0800

    December 6, part 1

---
+++ 2021/6/1/main.cpp
@@ -0,0 +1,38 @@
+#include <deque>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+int main() {
+	std::string data;
+	std::ifstream file;
+
+	file.open("2021/6/input.txt");
+	if (!file) {
+		std::cerr << "Failed to open input file." << std::endl;
+		return 1;
+	}
+
+	std::deque<int> nums;
+	while (file >> data) {
+		std::stringstream ss(data);
+		while (std::getline(ss, data, ',')) nums.push_back(std::stoi(data));
+	}
+	file.close();
+
+	for (int i = 0; i < 80; ++i) {
+		std::deque<int> new_nums;
+
+		for (int j = 0;  j < nums.size(); ++j) {
+			if (nums[j] == 0) {
+				nums[j] = 6;
+				new_nums.push_back(8);
+				continue;
+			}
+			--nums[j];
+		}
+		for (int &new_fish : new_nums) nums.push_back(new_fish);
+	}
+
+	std::cout << nums.size() << std::endl;
+}
