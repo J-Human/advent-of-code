6e712a8a5a06353978d48cda71db5d3578c98b35
J-Human <justinleeong@gmail.com>
2021-12-09 20:51:00 +0800

    December 8, part 1

---
+++ 2021/8/1/main.cpp
@@ -0,0 +1,28 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "../../../vendor/strutil.h"
+
+int main() {
+	std::string data;
+	std::ifstream file;
+
+	file.open("2021/8/input.txt");
+	if (!file) {
+		std::cerr << "Failed to open input file." << std::endl;
+		return 1;
+	}
+
+	int count = 0;
+	while (std::getline(file, data)) {
+		std::vector<std::string> sides = strutil::split(data, " | ");
+		std::vector<std::string> chars = strutil::split(sides.back(), " ");
+		for (std::string &v : chars)
+			if (v.size() <= 4 || v.size() == 7) count++;
+	}
+	file.close();
+
+	std::cout << count << std::endl;
+}
