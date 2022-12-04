68f2b86ad91f59a54a82a093fdafa5693ccf016f
J-Human <justinleeong@gmail.com>
2022-12-04 05:50:05 +0000

    2022 Day 4, Part 1-2

---
+++ 2022/04/1/main.py
@@ -0,0 +1,12 @@
+import os.path
+
+total = 0
+
+with open(os.path.dirname(__file__) + "/../input.txt") as file:
+	for pair in [[list(map(int, bounds.split("-"))) for bounds in pair.split(",")] for pair in file.read().splitlines()]:
+		sequences = [set(range(bounds[0], bounds[1] + 1)) for bounds in pair]
+
+		if sequences[0] & sequences[1] in sequences:
+			total += 1
+				
+print(total)
