148339d2150fddcf457d46918c4163aacd2d800d
J-Human <justinleeong@gmail.com>
2022-12-06 11:08:56 +0000

    Fix range end

--- 2022/06/1/main.py
+++ 2022/06/1/main.py
@@ -3,7 +3,7 @@ import os.path
 with open(os.path.dirname(__file__) + "/../input.txt") as file:	
 	buffer = file.read()
 
-	for i in range(0, len(buffer) + 1):
+	for i in range(0, len(buffer) - 3):
 		if len(set(buffer[i:i + 4])) == 4:
 			print(i + 4)
 			break
--- 2022/06/2/main.py
+++ 2022/06/2/main.py
@@ -3,7 +3,7 @@ import os.path
 with open(os.path.dirname(__file__) + "/../input.txt") as file:	
 	buffer = file.read()
 	
-	for i in range(0, len(buffer) + 1):
+	for i in range(0, len(buffer) - 13):
 		if len(set(buffer[i:i + 14])) == 14:
 			print(i + 14)
 			break
