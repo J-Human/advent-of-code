d95c6d0a38c5356573f165b01eb2d7895fd9690a
J-Human <justinleeong@gmail.com>
2022-12-06 07:22:33 +0000

    2022 Day 6, Part 1-2

---
+++ 2022/06/1/main.py
@@ -0,0 +1,9 @@
+import os.path
+
+with open(os.path.dirname(__file__) + "/../input.txt") as file:	
+	buffer = file.read()
+
+	for i in range(0, len(buffer) + 1):
+		if len(set(buffer[i:i + 4])) == 4:
+			print(i + 4)
+			break
