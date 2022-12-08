dda8815924a538a241d1669dc74245100e01d03a
J-Human <justinleeong@gmail.com>
2022-12-08 03:38:51 +0000

    2022 Day 7, Part 1-2

---
+++ 2022/07/1/main.py
@@ -0,0 +1,28 @@
+import collections
+import os.path
+import re
+	
+with open(os.path.dirname(__file__) + "/../input.txt") as file:
+	cwd = []
+	fs = collections.defaultdict(int)
+
+	for line in file.read().splitlines():
+		if line[0] == "$":
+			full_command = line.split(" ")[1:]
+
+			if full_command[0] == "cd":
+				destination = full_command[1]
+
+				if destination == "..":
+					cwd.pop()
+				elif destination == "/":
+					cwd = ["/"]
+				else:
+					cwd.append(destination + "/")
+		elif re.match("\d+", line[0]):
+			parent_cwd = cwd[:]
+			while len(parent_cwd):
+				fs["".join(parent_cwd)] += int(line.split(" ")[0])
+				parent_cwd.pop()
+
+	print(sum([n for n in fs.values() if n <= 1e5]))
